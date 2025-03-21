c8c223d9c818fcc5422ed92e4b1fdf63ced06511
Roman-Nazymko <168586482+Roman-Nazymko@users.noreply.github.com>
2025-03-21 16:32:36 +0100

    added Loader and Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
+import Loader from "./components/Loader/Loader";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 function App() {
   return (
     <>
       <Layout />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
