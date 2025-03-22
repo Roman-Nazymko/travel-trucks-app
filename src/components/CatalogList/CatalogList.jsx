923cbf89c070d530d14d520e60dae350f3a1d996
Roman-Nazymko <168586482+Roman-Nazymko@users.noreply.github.com>
2025-03-22 20:00:32 +0100

    added pagination

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -1,6 +1,14 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import toast, { Toaster } from "react-hot-toast";
-import { selectError, selectItems } from "../../redux/trucks/selectors";
+import clsx from "clsx";
+import {
+  selectError,
+  selectLoading,
+  selectItems,
+} from "../../redux/trucks/selectors";
+import { activateLoader } from "../../redux/trucks/slice";
+import { selectPaginationPage } from "../../redux/pagination/selectors";
+import { addValue } from "../../redux/pagination/slice";
 
 import CatalogTruckCard from "../../components/CatalogTruckCard/CatalogTruckCard";
 
@@ -8,7 +16,18 @@ import css from "./CatalogList.module.css";
 
 const CatalogList = () => {
   const trucks = useSelector(selectItems);
+  const visibleCount = useSelector(selectPaginationPage);
+  const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
+  const dispatch = useDispatch();
+
+  const loadMore = () => {
+    dispatch(activateLoader(true));
+    setTimeout(() => {
+      dispatch(addValue(4));
+      dispatch(activateLoader(false));
+    }, 500);
+  };
 
   if (error) {
     toast.error("An error occurred while fetching trucks.", {
@@ -19,12 +38,23 @@ const CatalogList = () => {
   return (
     <>
       <ul className={css.catalogList}>
-        {trucks.map((truck) => (
+        {trucks.slice(0, visibleCount).map((truck) => (
           <li className={css.catalogListItem} key={truck.id}>
             <CatalogTruckCard truck={truck} />
           </li>
         ))}
       </ul>
+      {visibleCount < trucks.length && (
+        <button
+          onClick={loadMore}
+          className={clsx(
+            visibleCount >= trucks.length ? css.disLoadMore : css.loadMore
+          )}
+          disabled={loading}
+        >
+          Load more
+        </button>
+      )}
 
       {error && <Toaster />}
     </>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import allTrucksReducer from "./trucks/slice.js";
+import paginationReducer from "./pagination/slice";
 
 export const store = configureStore({
   reducer: {
     allTrucks: allTrucksReducer,
+    pagination: paginationReducer,
   },
 });
