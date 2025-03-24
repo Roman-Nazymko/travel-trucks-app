4d97e92fcd3c4d182c11449267aaf81cf6fb8eee
Roman-Nazymko <168586482+Roman-Nazymko@users.noreply.github.com>
2025-03-24 13:32:40 +0100

    added features and reviews

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage"));
 const DetailsTruckPage = lazy(() =>
   import("./pages/DetailsTruckPage/DetailsTruckPage")
 );
+const Features = lazy(() => import("./components/Features/Features"));
+const Reviews = lazy(() => import("./components/Reviews/Reviews"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 function App() {
@@ -19,7 +21,11 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:truckId" element={<DetailsTruckPage />} />
+          <Route path="/catalog/:truckId" element={<DetailsTruckPage />}>
+            <Route index element={<Features />} />
+            <Route path="features" element={<Features />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
